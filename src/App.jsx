f1d2c51e0f7651bc4a41c6c6109f662dbc1a76ec
shiras-slipstream <shiras.astabits@gmail.com>
2024-04-28 23:49:54 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+function App() {
+  const [quote, setQuote] = useState("");
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    getQuote()
+  }, [])
+
+
+  const nextQuote = () => {
+    setLoading(true)
+    getQuote()
+  }
+  const getQuote = () => {
+    axios.get("https://api.adviceslip.com/advice").then((response) => {
+      const { advice } = response.data.slip
+      console.log(advice);
+      setQuote(advice)
+      setLoading(false)
+    })
+      .catch((error) => {
+        setLoading(false)
+        console.log(error);
+      })
+  }
+  return (
+    <>
+      <div className="app">
+        <div className="card">
+          {
+            loading && <div className="loading"></div>}
+          <div className="quoteTxt">
+            {!loading && <h2>{quote}</h2>}
+          </div>
+
+          <button disabled={loading} onClick={nextQuote} className={` ${loading ? 'disableBtn' : 'button'}`}>
+            {
+              loading && <div className="loading"></div>}
+            {!loading && <span>Motivate me🔥</span>}
+          </button>
+        </div>
+      </div>
+      <div className="copyright">
+        <span>❤️ shiras VM</span>
+      </div>
+    </>
+  )
+}
+
+export default App
